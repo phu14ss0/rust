e63b35c2631f578d0fe1a466ac3cae2a4758cb60
phu14ss0 <leewodyd0312@naver.com>
2022-04-10 23:36:45 +0900

    d2

---
+++ guessing_game/target/debug/deps/guessing_game-f4e5001666319c9f.d
@@ -0,0 +1,5 @@
+/Users/phulasso/Desktop/programming/rust/guessing_game/target/debug/deps/guessing_game-f4e5001666319c9f: src/main.rs
+
+/Users/phulasso/Desktop/programming/rust/guessing_game/target/debug/deps/guessing_game-f4e5001666319c9f.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/phulasso/Desktop/programming/rust/guessing_game/target/debug/guessing_game: /Users/phulasso/Desktop/programming/rust/guessing_game/src/main.rs
