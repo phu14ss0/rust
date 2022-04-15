f8e601f4584ae6867456fb2df31286f3bbfbe455
phu14ss0 <leewodyd0312@naver.com>
2022-04-15 18:59:01 +0900

    d6

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,7 +2,7 @@ extern crate rand;
 
 use std::io; // input/output 라이브러리를 스코프로 가져옴 11
 use rand::Rng;  // Rng는 메소드들이 정의되어 있는 trait
-use std::cmp::Ordering;
+use std::cmp::Ordering; // Ordering은 열거형 Less, Greater, Equal 값을 가짐
 
 fn main() {
     println!("Guess the number!");  // 문자열을 화면에 표시하는 매크로
@@ -24,7 +24,8 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
+    let guess: u32 = guess.trim().parse()   // 정수형 u32로 guess 변수를 재사용함
+                                            // parse()메소드는 문자열을 숫자형으로 파싱함
         .expect("Please type a number!");
     
     // {}는 변경자로 값이 표시되는 위치를 나타냄(python format과 비슷)
@@ -35,7 +36,7 @@ fn main() {
     // Cargo.lock은 처음 프로그램을 빌드할 때 기준을 만족하는 모든 의존 패키지 버전을 확인하고 Cargo.lock에 기록, 이후 빌드할 때 그 안에 명시된 버전을 사용한다.
     // cargo update 명령으로 사용중인 크레이트의 업데이트된 버전이 있으면 업데이트 함
 
-    match guess.cmp(&secret_number) {
+    match guess.cmp(&secret_number) {   // cmp 메소드는 두 값을 비교하며 비교 가능한 모든 것들에 대한 호출 
         Ordering::Less => println!("Too small"),
         Ordering::Greater => println!("Too big"),
         Ordering::Equal => println!("You win!"),
